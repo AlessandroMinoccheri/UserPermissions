d1f1246e5188540b9aefa44ce180808c00c234bf
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2015-01-01 12:23:08 +0100

    fix some indent line

--- Controller/Component/UserPermissionsComponent.php
+++ Controller/Component/UserPermissionsComponent.php
@@ -2,31 +2,46 @@
 App::uses('Component', 'Controller', 'Session');
 
 class UserPermissionsComponent extends Component {
-	var $controller = '';
-    var $components = array('RequestHandler');
 
-	/* for old version of cakephp < 2.4.0 
+/**
+ * Controller name
+ *
+ * @var string
+ */
+	public $controller = '';
+
+/**
+ * Components array
+ *
+ * @var array
+ */
+   	public $components = array('RequestHandler');
+
+
+	/* 
+	for old version of cakephp < 2.4.0 
 	function initialize(&$controller, $settings = array()) { 
         $this->controller =& $controller; 
     } 
     */
 
-    /**
-	 * Initialization to get controller variable
-	 *
-	 * @param Controller $controller The controller to use.
-	 * @param array $settings Array of settings.
-	 */
+
+/**
+* Initialization to get controller variable
+*
+* @param Controller $controller The controller to use.
+* @param array $settings Array of settings.
+*/
     function initialize (Controller $controller, $settings = array()) { 
 		$this->controller =& $controller; 
 	}
 
-	/**
-	 * Initialization to get controller variable
-	 *
-	 * @param array $rules Array of rules for permissions.
-	 * @return string '0' if user / group doesn't have permission, 1 if has permission
-	 */
+/**
+* Initialization to get controller variable
+*
+* @param array $rules Array of rules for permissions.
+* @return string '0' if user / group doesn't have permission, 1 if has permission
+*/
     public function allow ($rules) {
     	App::uses('CakeSession', 'Model/Datasource');
 		$userId = CakeSession::read('Auth.User.id');
@@ -80,8 +95,9 @@ class UserPermissionsComponent extends Component {
 						if(!in_array($action, $actions)){
 							$find = 1;
 							if($redirect != ''){
-								if($message != '')
+								if($message != ''){
 									$this->controller->Session->setFlash($message);
+								}
 								
 								$this->controller->redirect($redirect);
 							}
@@ -99,8 +115,9 @@ class UserPermissionsComponent extends Component {
 				if($key == $action){
 					if(!$this->controller->$value()){
 						if($redirect != ''){
-							if($message != '')
+							if($message != ''){
 								$this->controller->Session->setFlash($message);
+							}
 							
 							$this->controller->redirect($redirect);
 						}
